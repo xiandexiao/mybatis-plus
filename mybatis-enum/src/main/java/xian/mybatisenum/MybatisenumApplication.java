9bca1ea31e5892f67d6f0ed10c0c18606894fff7
xiandexiao <zangxiaoxian@qq.com>
2018-04-11 09:04:29 +0800

    创建mybatis-enum项目

---
+++ mybatis-enum/src/main/java/xian/mybatisenum/MybatisenumApplication.java
@@ -0,0 +1,12 @@
+package xian.mybatisenum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisenumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisenumApplication.class, args);
+	}
+}
---
+++ mybatis-enum/src/test/java/xian/mybatisenum/MybatisenumApplicationTests.java
@@ -0,0 +1,16 @@
+package xian.mybatisenum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisenumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
